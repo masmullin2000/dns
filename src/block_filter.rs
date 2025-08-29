51733de7ef72c1f559bd38611936ed70c207fc35
Michael Mullin <masmullin@gmail.com>
2025-08-29 16:49:14 -0400

    switch to foldhash

--- src/block_filter.rs
+++ src/block_filter.rs
@@ -2,24 +2,41 @@ use std::hash::{Hash, Hasher};
 
 use std::collections::HashSet;
 
-use ahash::{AHasher, RandomState};
 use anyhow::Result;
+use foldhash::{
+    SharedSeed,
+    fast::{FixedState, FoldHasher},
+};
 use tracing::{error, warn};
 
-#[derive(Debug, Default)]
+static SHARED_SEED: SharedSeed = SharedSeed::from_u64(0);
+
+fn hash_str(name: &str) -> u64 {
+    let mut hasher = FoldHasher::with_seed(0, &SHARED_SEED);
+    name.hash(&mut hasher);
+    hasher.finish()
+}
+
+#[derive(Debug)]
 pub struct BlockFilter {
-    set: HashSet<u64, RandomState>,
+    set: HashSet<u64, FixedState>,
+}
+
+impl Default for BlockFilter {
+    fn default() -> Self {
+        Self {
+            set: HashSet::with_hasher(FixedState::with_seed(0)),
+        }
+    }
 }
 
 impl BlockFilter {
-    fn set(&mut self, set: HashSet<u64, RandomState>) {
+    fn set(&mut self, set: HashSet<u64, FixedState>) {
         self.set = set;
     }
 
     pub fn contains(&self, name: &str) -> bool {
-        let mut hasher = AHasher::default();
-        name.hash(&mut hasher);
-        let val = hasher.finish();
+        let val = hash_str(name);
         self.set.contains(&val)
     }
 
@@ -29,7 +46,7 @@ impl BlockFilter {
 }
 
 #[derive(Default, Debug)]
-pub struct BlockFilterBuilder(std::collections::HashSet<String, RandomState>);
+pub struct BlockFilterBuilder(HashSet<String, FixedState>);
 
 impl BlockFilterBuilder {
     // set all the items in a blocklist file
@@ -59,16 +76,8 @@ impl BlockFilterBuilder {
         } else {
             let mut block_filter = BlockFilter::default();
 
-            let block_coll = self
-                .0
-                .iter()
-                .map(|s| {
-                    // Hash the string to a u64 for use in the Set
-                    let mut hasher = AHasher::default();
-                    s.hash(&mut hasher);
-                    hasher.finish()
-                })
-                .collect();
+            // Hash the strings to a u64 for use in the Set
+            let block_coll = self.0.iter().map(|s| hash_str(s)).collect();
             block_filter.set(block_coll);
             block_filter
         }
